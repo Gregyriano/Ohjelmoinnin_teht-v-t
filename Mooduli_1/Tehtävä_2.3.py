71e7a1cb3baa9cebe9fbde0a8c89bcce6ca854fb
Gregyriano <grigoril@metropolia.fi>
2025-08-26 17:05:28 +0300

    2.3 tehtävä

---
+++ Mooduli_1/Tehtävä_2.3.py
@@ -0,0 +1,10 @@
+import math
+
+suorakulmion_korkeus = input("Anna suorakulmion korkeus:")
+
+suorakulmion_kanna = input("Anna suorakulmi kanna:")
+
+suorakulmion_piiri = 2 * ( float(suorakulmion_kanna) + float(suorakulmion_korkeus) )
+
+print("Tämä on sinun suorakulmion piiri: " + str(suorakulmion_piiri))
+
