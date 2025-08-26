8907d398a9796429a0d1c09a01c56dae5fd8d095
Gregyriano <grigoril@metropolia.fi>
2025-08-26 20:25:05 +0300

    2.6 tehtävä

---
+++ Mooduli_1/tehtävä_2.6.py
@@ -0,0 +1,13 @@
+import random
+
+koodi1_numero_1 = random.randint(0, 9)
+koodi1_numero_2 = random.randint(0, 9)
+koodi1_numero_3 = random.randint(0, 9)
+
+koodi2_numero_1 = random.randint(1, 6)
+koodi2_numero_2 = random.randint(1, 6)
+koodi2_numero_3 = random.randint(1, 6)
+koodi2_numero_4 = random.randint(1, 6)
+
+print(f"ensimmainen koodi {koodi1_numero_1, koodi1_numero_2, koodi1_numero_3}")
+print(f"toinen koodi {koodi2_numero_1, koodi2_numero_2, koodi2_numero_3, koodi2_numero_4}")
