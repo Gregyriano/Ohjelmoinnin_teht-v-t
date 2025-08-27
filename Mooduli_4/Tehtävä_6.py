484dbbde03c3d941cebbf736cb1836b0e9d01fc0
Gregyriano <grigoril@metropolia.fi>
2025-08-27 11:35:33 +0300

    Mod_3

--- Mooduli_4/Tehtävä_6.py
+++ Mooduli_4/Tehtävä_6.py
@@ -1,12 +1,18 @@
 import random
 
 # kaikkien pistojen määrä
-N = 1000
-i = 0
+N = float(input("Anna suuri luku: "))
+n = i = 0
+
 while i < N:
     i += 1
     # arvotaan satunnainen piste koordinatistoon
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
     print(f"Arvotuu piste: {x}, {y}")
-    # TODO: testaa, toteuttaako piste epäyhtälön x*x + y*y < 1
+
+    if x*x + y*y < 1:
+        n += 1
+    # jos ehto on totta, kasvata n-laskurin arvoa
+
+print(f"Pi:n liukuluku ≈ {4 * n / N:0.4f}.")
