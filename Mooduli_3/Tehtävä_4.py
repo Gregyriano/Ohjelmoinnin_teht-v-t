06e9141f1b6d391e3ab46a4404367d6d476b2a6d
Gregyriano <grigoril@metropolia.fi>
2025-08-31 21:40:28 +0300

    dsaf

--- Mooduli_3/Tehtävä_4.py
+++ Mooduli_3/Tehtävä_4.py
@@ -3,6 +3,6 @@ lukuvuosi = float(input("Anna lukuvuosi: "))
 if (lukuvuosi % 4 == 0):
     print("Tämä vuosi on karkainen.")
 elif (lukuvusi % 100 == 0 and lukuvuosi % 400 == 0 ):
-print("Tämä vuosi on karkainen.")
+    print("Tämä vuosi on karkainen.")
 else:
     print("Tämä vuosi ei ole karkainen.")
---
+++ Mooduli_4/Tehtäva_1_mod_4.py
@@ -0,0 +1,7 @@
+N = 1000
+i = 0
+
+while i < N:
+    if i % 3 == 0:
+        print("i")
+    i =+ 1
