d962b853526fab84470e259ca92cbb11af45ae7a
Gregyriano <grigoril@metropolia.fi>
2025-08-31 21:29:45 +0300

    Karkausvuosi tehtävä

---
+++ Moduuli_3/Tehtävä_4.py
@@ -0,0 +1,8 @@
+lukuvuosi = float(input("Anna lukuvuosi: "))
+
+if (lukuvuosi % 4 == 0):
+    print("Tämä vuosi on karkainen.")
+elif (lukuvusi % 100 == 0 and lukuvuosi % 400 == 0 ):
+print("Tämä vuosi on karkainen.")
+else:
+    print("Tämä vuosi ei ole karkainen.")
